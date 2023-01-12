66b5283a07635adf96139587d8db8ff2de566ccf
Ihnatenko Vitalii <Ihnatenko Vitalii@DESKTOP-DGTQ2P4>
2023-01-12 11:19:08 +0200

    Добавьте файлы проекта.

---
+++ lab-3/lab-3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>  
+using namespace std;
+void main()
+{
+
+    int a;
+    setlocale(LC_ALL, "Ukr");
+    cout << "Enter 1, 2 or 3: ";
+    cin >> a;
+    switch (a)
+    {
+    case 1:
+        cout << "You entered 1"; break;
+    case 2:
+        cout << "You entered 2"; break;
+    case 3:
+        cout << "You entered 3"; break;
+    default:
+        cout << "You did not enter 1, 2 or 3"; break;
+    }
+}
